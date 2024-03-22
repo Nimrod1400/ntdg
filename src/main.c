e5c2ad40ba3d50084ab6491225cbdf438c5b4b4b
shluha <shamray@bk.ru>
2024-03-22 23:15:38 +0700

    Successfully tested Termbox2

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #define TB_IMPL
 #include "termbox2.h"
+
 #include <stdio.h>
+#include <stdint.h>
 
 #define MIN_WIDTH  200
 #define MIN_HEIGHT 60
@@ -11,24 +13,28 @@ typedef struct {
 } vector2;
 
 typedef struct {
-	struct vector2 pos;
-	struct vector2 size;
+	vector2 pos;
+	vector2 size;
 } rectangle;
 
 typedef struct {
-	unsigned byte color;
-	char symbol;
+	uintattr_t color;
+	uint32_t symbol;
 } pixel;
 
 typedef struct {
-  int w, h;
+  int width, height;
 	pixel *pixels;
 } screen;
 
-void draw_screen(*struct screen scr) {
-  for (int x = 0; x < scr->width; ++x) {
-		for (int y = 0; y < scr->height; ++y) {
-			tb_printf(x, y, scr->pixels[y * scr->width + x].color, 0, scr->pixels[y * scr->width + x].symbol);
+void draw_screen(screen *scr) {
+  int w = scr->width;
+  int h = scr->height;
+
+  for (int x = 0; x < w; ++x) {
+		for (int y = 0; y < h; ++y) {
+  		pixel p = scr->pixels[y * scr->width + x];
+  		tb_set_cell(x, y, p.symbol, p.color, 0);
 		}
   }
 
@@ -36,6 +42,7 @@ void draw_screen(*struct screen scr) {
 }
 
 int main(void) {
+  struct tb_event ev; 
 	tb_init();
 
 	int w = tb_width();
@@ -49,6 +56,21 @@ int main(void) {
 		exit(-1);
 	}
 
+	// uint32_t rect = 9646;
+
+	pixel *ps = malloc(w * h * sizeof(pixel));
+	for (int i = 0; i < w * h; ++i) {
+		ps[i].color = TB_BLUE;
+		ps[i].symbol = '+';
+	}
+
+	screen scr = { w, h, ps, };
+
+	draw_screen(&scr);
+	
+	tb_present();
+
+	tb_poll_event(&ev);
 	tb_shutdown();
 
 	return 0;
