e761fd614a5c27dd280217936f891c6ba77823d5
shluha <shamray@bk.ru>
2024-03-23 11:28:30 +0700

    Screen initializer

--- src/main.c
+++ src/main.c
@@ -7,6 +7,9 @@
 #define MIN_WIDTH  200
 #define MIN_HEIGHT 60
 
+#define BARS_CHAR  0
+#define BARS_COLOR 0
+
 typedef struct {
 	float x;
 	float y;
@@ -23,17 +26,64 @@ typedef struct {
 } pixel;
 
 typedef struct {
-  int width, height;
+  int swidth, sheight;
+  int rwidth, rheight;
+  int mleft, mright, mtop, mbottom;
 	pixel *pixels;
 } screen;
 
-void draw_screen(screen *scr) {
-  int w = scr->width;
-  int h = scr->height;
+void screen_init(screen *scr) {
+	int w = tb_width();
+	int h = tb_height(); 
 
-  for (int x = 0; x < w; ++x) {
-		for (int y = 0; y < h; ++y) {
-  		pixel p = scr->pixels[y * scr->width + x];
+	scr->swidth = w;
+	scr->sheight = h;
+
+	scr->rwidth = MIN_WIDTH;
+	scr->rheight = MIN_HEIGHT;
+
+	int ml, mr, mt, mb;
+
+	if ((w - MIN_WIDTH) % 2 == 0) {
+		ml = (w - MIN_WIDTH) / 2; 
+		mr = ml;
+	}
+	else {
+		ml = (w - MIN_WIDTH - 1) / 2;
+		mr = (w - MIN_WIDTH + 1) / 2;
+	}
+
+	if ((h - MIN_HEIGHT) % 2 == 0) {
+		mt = (h - MIN_HEIGHT) / 2;
+		mb = mt;
+	}
+	else {
+		mt = (h - MIN_HEIGHT + 1) / 2;
+		mb = (h - MIN_HEIGHT - 1) / 2; 
+	}
+
+	scr->mleft = ml;
+	scr->mright = mr;
+	scr->mtop = mr;
+	scr->mbottom = mb;
+
+	scr->pixels = calloc(MIN_WIDTH * MIN_HEIGHT, sizeof(pixel));
+}
+
+void screen_draw(screen *scr) {
+  int rw = scr->rwidth;
+  int rh = scr->rheight;
+
+  for (int x = scr->mleft + 1; x < rw; ++x) {
+		for (int y = scr->mtop + 1; y < rh; ++y) {
+  		pixel p = scr->pixels[y * rw + x];
+  		tb_set_cell(x, y, p.symbol, p.color, 0);
+		}
+  }
+
+  for (int x = 0; x < scr->mleft; ++x) {
+		for (int y = scr->mtop + 1; y < rh; ++y) {
+  		pixel p = scr->pixels[y * rw + x];
   		tb_set_cell(x, y, p.symbol, p.color, 0);
 		}
   }
