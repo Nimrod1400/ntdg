464320da892358f3d04c4e425ebf68172c6c4e79
shluha <shamray@bk.ru>
2024-03-28 17:28:25 +0700

    Screen and Event are now globals. Implementing main loop.

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@
 #define MIN_HEIGHT 90
 #define BARS_CHAR  0
 #define BARS_FG    0
-#define BARS_BG    0x000020
+#define BARS_BG    0x101030
 
 typedef uintattr_t pixel;
 
@@ -20,18 +20,18 @@ typedef struct {
   pixel *pixels; // array of pixels
 } screen;
 
-void screen_init(screen *scr) {
+static struct tb_event ev = { 0 };
+static screen scr = { 0 };
+
+void screen_init() {
   int w = tb_width();
   int h = tb_height(); 
 
-  assert(w > 0);
-  assert(h > 0);
+  scr.swidth = w;
+  scr.sheight = h;
 
-  scr->swidth = w;
-  scr->sheight = h;
-
-  scr->rwidth = MIN_WIDTH;
-  scr->rheight = MIN_HEIGHT;
+  scr.rwidth = MIN_WIDTH;
+  scr.rheight = MIN_HEIGHT;
 
   int ml, mr, mt, mb;
 
@@ -53,39 +53,40 @@ void screen_init(screen *scr) {
     mb = mt - 1; 
   }
 
-  scr->mleft = ml;
-  scr->mright = mr;
-  scr->mtop = mt;
-  scr->mbottom = mb;
-  int len = scr->rwidth * scr->rheight;
-  scr->pixels = malloc(len * sizeof(pixel));
+  scr.mleft = ml;
+  scr.mright = mr;
+  scr.mtop = mt;
+  scr.mbottom = mb;
+
+  int len = scr.rwidth * scr.rheight;
+  scr.pixels = malloc(len * sizeof(pixel));
 }
 
-void screen_draw_bars(const screen *scr) {
-  // Left margin
-  for (int x = 0; x < scr->mleft; ++x) {
-    for (int y = 0; y < scr->sheight; ++y) {
+void screen_draw_bars() {
+  // Left margin bar
+  for (int y = 0; y < scr.sheight; ++y) {
+    for (int x = 0; x < scr.mleft; ++x) {
       tb_set_cell(x, y, BARS_CHAR, BARS_FG, BARS_BG);
     }
   }
 
-  // Top margin
-  for (int x = 0; x < scr->swidth; ++x) {
-    for (int y = 0; y < scr->mtop; ++y) {
+  // Top margin bar
+  for (int y = 0; y < scr.mtop; ++y) {
+    for (int x = 0; x < scr.swidth; ++x) {
       tb_set_cell(x, y, BARS_CHAR, BARS_FG, BARS_BG);
     }
   }
 
-  // Right margin
-  for (int x = scr->swidth - scr->mright; x < scr->swidth; ++x) {
-    for (int y = 0; y < scr->sheight; ++y) {
+  // Right margin bar
+  for (int y = 0; y < scr.sheight; ++y) {
+    for (int x = scr.mright + scr.rwidth; x < scr.swidth; ++x) {
       tb_set_cell(x, y, BARS_CHAR, BARS_FG, BARS_BG);
     }
   }
 
-  // Bottom margin
-  for (int x = 0; x < scr->swidth; ++x) {
-    for (int y = scr->sheight - scr->mbottom; y < scr->sheight; ++y) {
+  // Bottom margin bar
+  for (int y = scr.mtop + scr.rheight; y < scr.sheight; ++y) {
+    for (int x = 0; x < scr.swidth; ++x) {
       tb_set_cell(x, y, BARS_CHAR, BARS_FG, BARS_BG);
     }
   }
@@ -93,11 +94,13 @@ void screen_draw_bars(const screen *scr) {
   tb_present();
 }
 
-void screen_draw_content(const screen *scr) {
-  int rw = scr->rwidth;
-  for (int x = scr->mleft; x < scr->swidth - scr->mright; ++x) {
-    for (int y = scr->mtop; y < scr->sheight - scr->mbottom; ++y) {
-      pixel p = scr->pixels[y * rw + x];
+void screen_draw_content() {
+  int rw = scr.rwidth;
+  int i = 0;
+
+  for (int y = scr.mtop; y < scr.mtop + scr.rheight; ++y) {
+    for (int x = scr.mleft; x < scr.mleft + scr.rwidth; ++x) {
+      pixel p = scr.pixels[i++];
       tb_set_cell(x, y, 0, 0, p);
     }
   }
@@ -105,22 +108,29 @@ void screen_draw_content(const screen *scr) {
   tb_present(); 
 }
 
-void screen_fill_content(screen *scr, pixel p) {
-  int rw = scr->rwidth;
-  int rh = scr->rheight;
+void screen_fill_content(pixel p) {
+  int rw = scr.rwidth;
+  int rh = scr.rheight;
 
   for (int i = 0; i < rw * rh; ++i) {
-    scr->pixels[i] = p;
+    scr.pixels[i] = p;
   }
 }
 
-int main(void) {
-  struct tb_event ev; 
+// Is using macros for this kind of one-line operations cool?
+// Like, instead of function call it will be inline operation, so it will be more efficient?
+#define screen_fill_pixel(p, x, y) scr.pixels[(y) * scr.rwidth + (x)] = (p);
 
-  tb_init();
+void screen_fill_rectangle(pixel p, int x1, int y1, int x2, int y2) {
+  for (int y = y1; y < y2; ++y) {
+    for (int x = x1; x < x2; ++x) {
+      screen_fill_pixel(p, x, y);
+    }
+  }
+}
 
-  tb_set_output_mode(TB_OUTPUT_TRUECOLOR);
-
+// Check if terminal is big enough
+void validate_term_size() {
   int w = tb_width();
   int h = tb_height(); 
 
@@ -132,16 +142,55 @@ int main(void) {
             MIN_WIDTH, MIN_HEIGHT, w, h);
     exit(-1);
   }
+}
+
+void receive_input() {
+  tb_poll_event(&ev);
+  if (ev.type == TB_EVENT_KEY) {
+
+    if (ev.key == TB_KEY_CTRL_C || ev.key == TB_KEY_ESC) {
+      tb_shutdown();
+      exit(0);
+    }
+  }
+}
+
+void update_state() {
+
+}
+
+void render() {
+  for (int y = 0; y < MIN_HEIGHT; ++y) {
+    for (int x = 0; x < MIN_WIDTH; ++x) {
+      screen_fill_pixel((x + 1) * (y + 1) * (0xffffff / 27000) % 0xffffff, x, y);
+    }
+  }
+
+  screen_draw_content();
+}
+
+int main(void) {
+  struct tb_event ev; 
+
+  tb_init();
+
+  validate_term_size();
+
+  tb_set_output_mode(TB_OUTPUT_TRUECOLOR);
 
   screen scr;
-  screen_init(&scr);
+  screen_init();
 
-  screen_fill_content(&scr, 0xdd00dd);
+  screen_draw_bars();
+  render();
 
-  screen_draw_content(&scr);
-  screen_draw_bars(&scr);
+  // Main loop
+  for (;;) {
+    receive_input(&ev);
+    update_state();
+    render(&scr);
+  }
 
-  tb_poll_event(&ev);
   tb_shutdown();
 
   return 0;
